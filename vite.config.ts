5ee600b5713ab1849fb12aee737300159aad039d
Copheles <xixxykitkit@gmail.com>
2026-07-18 04:05:09 +0630

    Changed to worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -8,7 +9,7 @@ const rootDir = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [cloudflare(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(rootDir, "./src"),
